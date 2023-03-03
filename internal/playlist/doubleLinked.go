eeb464ede15260a7c1c41c34b6f23318bd9ba750
elena <edementieva@list.ru>
2023-03-03 17:01:56 +0300

    add playlist

--- internal/playlist/doubleLinked.go
+++ internal/playlist/doubleLinked.go
@@ -16,46 +16,50 @@ type doubleLinkedList struct {
 
 func newDoubleLinkedList() storage {
 	return &doubleLinkedList{
-		track: list.New(),
+		track:       list.New(),
+		currntTrack: nil,
 	}
 }
-func (d *doubleLinkedList) Firstrack (ctx context.Context) (*Song,  error) {
-	t  := d.track.Front()
-	if t != nil {
-		return nil, errors.New("")
+
+// Firstrack возвращает первый элемент из списка
+func (d *doubleLinkedList) Firstrack(ctx context.Context) (*Song, error) {
+	d.currntTrack = d.track.Front()
+	if d.currntTrack == nil {
+		return &Song{}, errors.New("playlist is empty")
 	}
-	track := t.Value.(*Song)
+	track := d.currntTrack.Value.(*Song)
 	return track, nil
 }
 
-func (d *doubleLinkedList) currentTrack(ctx context.Context, *Song){
-	
-}
-
-func (d *doubleLinkedList) PushBack(ctx context.Context, song Song) error {
+// PushBack вставляет новый элемент в конец списка
+func (d *doubleLinkedList) PushBack(ctx context.Context, song Song) *Song {
 	d.lock.Lock()
 	defer d.lock.Unlock()
 
-	d.track.PushBack(song)
-	return nil
+	t := d.track.PushBack(song)
+	track := t.Value.(*Song)
+	return track
 }
 
-func (d *doubleLinkedList) NextSong(ctx context.Context) error {
+// NextSong возвращает следующий элемент из списка
+func (d *doubleLinkedList) NextSong(ctx context.Context) (*Song, error) {
 	d.lock.Lock()
 	defer d.lock.Unlock()
 
 	if d.track.Len() == 0 {
-		return errors.New("playlist is empty")
+		return nil, errors.New("playlist is empty")
 	}
 	d.currntTrack = d.currntTrack.Next()
+	track := d.currntTrack.Value.(*Song)
 
-	return nil
+	return track, nil
 }
 
-func (d *doubleLinkedList) PrevSong(ctx context.Context, song Song) error {
+// PrevSong возвращает предыдущий элемент из списка
+func (d *doubleLinkedList) PrevSong(ctx context.Context) error {
 	d.lock.Lock()
 	defer d.lock.Unlock()
-	
+
 	d.currntTrack = d.currntTrack.Prev()
 	if d.currntTrack == nil {
 		d.currntTrack = d.track.Back()
--- internal/playlist/playlist.go
+++ internal/playlist/playlist.go
@@ -10,7 +10,6 @@ type Song struct {
 	ID       int64
 	Name     string
 	Duration time.Duration
-	Status   string
 }
 
 type playlist struct {
@@ -20,47 +19,59 @@ type playlist struct {
 
 type storage interface {
 	Firstrack(ctx context.Context) (*Song, error)
-	PushBack(ctx context.Context, song Song) error
-	NextSong(ctx context.Context) error
-	PrevSong(ctx context.Context, s Song) error
+	PushBack(ctx context.Context, song Song) *Song
+	NextSong(ctx context.Context) (*Song, error)
+	PrevSong(ctx context.Context) error
 }
 
 type Playlist interface {
 	Play(ctx context.Context) (*Song, error)
 	Pause(ctx context.Context) error
-	AddSong(ctx context.Context, s Song) error
-	Next(ctx context.Context) error
-	Prev(ctx context.Context, s Song) error
+	AddSong(ctx context.Context, s Song) *Song
+	Next(ctx context.Context) (*Song, error)
+	Prev(ctx context.Context) error
 }
 
 func NewPlaylust() Playlist {
 	return &playlist{}
 }
 
+// Play начинает воспроизведение
 func (p *playlist) Play(ctx context.Context) (*Song, error) {
-	for track, err := p.storage.Firstrack(ctx); err != nil; err = p.Next(ctx) {
-		d.currntTrack
-		return track, nil
+	track, err := p.storage.Firstrack(ctx)
+	if err != nil {
+		return &Song{}, errors.New("playlist is empty")
 	}
-	return &Song{}, nil
+	track, err = p.storage.NextSong(ctx)
+	if err != nil {
+		return &Song{}, errors.New("playlist is empty")
+	}
+	return track, nil
 }
 
+// Pause приостанавливает воспроизведение
 func (p *playlist) Pause(ctx context.Context) error {
 	return nil
 }
-func (p *playlist) AddSong(ctx context.Context, s Song) error {
-	err := p.storage.PushBack(ctx, s)
-	if err != nil {
-		return errors.New("can't add song")
-	}
-	return nil
-}
-func (p *playlist) Next(ctx context.Context) error {
 
-	return nil
+// AddSong добавляет в конец плейлиста песню
+func (p *playlist) AddSong(ctx context.Context, s Song) *Song {
+	track := p.storage.PushBack(ctx, s)
+	return track
 }
-func (p *playlist) Prev(ctx context.Context, s Song) error {
-	err := p.storage.PrevSong(ctx, s)
+
+// Next воспроизвести след песню
+func (p *playlist) Next(ctx context.Context) (*Song, error) {
+	track, err := p.storage.NextSong(ctx)
+	if err != nil {
+		return &Song{}, errors.New("playlist is empty")
+	}
+	return track, nil
+}
+
+// Prev воспроизвести предыдущую песню
+func (p *playlist) Prev(ctx context.Context) error {
+	err := p.storage.PrevSong(ctx)
 	if err != nil {
 		return errors.New("can't find previous song")
 	}
